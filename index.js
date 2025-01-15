655a0bf81514f1df8a055884855586c1a9636410
YuriSinoca <sinocayuri@gmail.com>
2025-01-15 14:33:22 -0300

    user_api

--- index.js
+++ index.js
@@ -1,75 +1,90 @@
 
-const express = require ('express')
+const express = require('express')
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 
-let users = []
-let nextId = 1
 
 //Rota para retornar todos os usuários
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
+    const users = await prisma.user.findMany()
     res.json(users)
 })
+
+
+
 // Rota para criar usuários 
-app.post("/users", (req, res) => {
-    const {name, email} = req.body;
+app.post("/users", async (req, res) => {
+    const { name, email } = req.body;
 
-    if(!name || !email){
-        return res.status(400).json({error:"Name e email são obrigatorio"})
+    if (!name || !email) {
+        return res.status(400).json({ error: "Name e email são obrigatorio" })
     }
 
-    const newUser = {
-        id: nextId++,
-        name: name, 
-        email: email
-    }
+    const user = await prisma.user.create({
+        data: {
+            name,
+            email
+        }
+    })
 
-    users.push(newUser)
-    res.status(201). json(newUser)
+    res.status(201).json(user)
 })
-//Rota para busca um usúario pelo ID
-app.get("/user/:id", (req, res) => {
-    const { id } = req.params
-    const user = users.find((u) => u.id === parseInt(id))
 
-    if(!user){
-        return res.status(404).json({error: "Usuário não encontrado"})
+
+
+//Rota para busca um usúario pelo ID
+app.get("/user/:id", async (req, res) => {
+    const id = parseInt(req.params.id)
+    const user = await prisma.user.findUnique({ where: { id } })
+
+    if (!user) {
+        return res.status(404).json({ error: "Usuário não encontrado" })
     }
 
     res.json(user)
 })
 
 //Rota para deletar um usuário pelo ID
-app.delete("/user/:id", (req, res) => {
-    const { id } = req.params
-    const user = users.find((u) => u.id === parseInt(id))
+app.delete("/user/:id", async (req, res) => {
+    const id = parseInt(req.params.id)
+    const user = await prisma.user.findUnique({ where: { id } })
 
-    if(!user){
-        return res.status(404).json({error: "Usuário não encontrado"})
+    if (!user) {
+        return res.status(404).json({ error: "Usuário não encontrado" })
     }
-    users = users.filter((u) => u.id != id)
-    res.status(204).send
+
+    await prisma.user.delete({
+        where: {id}
+    })
+
+    res.status(204).send()
 })
 
+
 //Rota para atualizar o usuário pelo ID
-app.put("/user/:id", (req, res) => {
-    const { id } = req.params
-    const user = users.find((u) => u.id == id)
+app.put("/user/:id", async (req, res) => {
+    const id = parseInt(req.params.id)
+    const user = await prisma.user.findUnique({ 
+        where: { id } 
+    })  
 
-    if(!user){
-        return res.status(404).json({error: "Usuário não encontrado"})
+    if (!user) {
+        return res.status(404).json({ error: "Usuário não encontrado" })
     }
-    const {name, email} = req.body
+    const { name, email } = req.body
 
-    if(name){ 
-        user.name = name 
-     }
-    
-    if(email){
-        user.email = email 
-    }
+    const updatedUser = await prisma.user.update({
+        where: {id},
+        data: {
+            name,
+            email
+        }
+    })
 
-    res.json(user)
+    res.json(updatedUser)
 
 })
 
